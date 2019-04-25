400eec29b3bf6b976dae79f1b58524d57362449b
MaleWeb <896226896@qq.com>
2019-04-25 15:14:31 +0800

    测试travis集成

--- deploy.sh
+++ deploy.sh
@@ -21,8 +21,8 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 let access_token = "6ljH3PgKi4CiT6RWDRtSNw";
-# git push -f https://${access_token}@github.com/fewalksideways/FeSidle.git master:gh-pages  
+git push -f https://${access_token}@github.com/fewalksideways/FeSidle.git master:gh-pages  
 
-git push -f git@github.com:fewalksideways/FeSidle.git master:gh-pages
+# git push -f git@github.com:fewalksideways/FeSidle.git master:gh-pages
 
 cd -
